06854d48809e861cba0ebc7eb7fa3da8b60c716c
Carlos Guilherme Miranda Finelli <60704748+CarlosFinelli@users.noreply.github.com>
2020-11-18 16:32:42 -0300

    Finalização de cases
    Finalização do case 1, do case 4 e do case 5;

---
+++ SA2_Carlos/SA2_Carlos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SA2_Carlos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SA2_Carlos/SA2_Carlos/Receitas.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SA2_Carlos
+{
+    class Receitas
+    {
+    }
+}
